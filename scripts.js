67a26d1c2d2b30769507a8ad7e092f29b1a59ac0
Bryson Taylor <btay171@wgu.edu>
2024-01-19 15:49:08 -0800

    Added in calendar panel and day functionality.

--- scripts.js
+++ scripts.js
@@ -22,7 +22,8 @@ for (let i = 0; i < hands.length; i++) {
 }
 
 // const nums = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-const nums = [12, "|", "|", 3, "|", "|", 6, "|", "|", 9, "|", "|"];
+// const nums = [12, "|", "|", 3, "|", "|", 6, "|", "|", 9, "|", "|"];
+const nums = ["XII", "|", "|", "III", "|", "|", "VI", "|", "|", "IX", "|", "|"];
 // const nums = ["XII", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI"];
 // const nums = ["XII", "I", "II", "III", "IIII", "V", "VI", "VII", "VIII", "IX", "X", "XI"];
 
@@ -60,6 +61,54 @@ const setAmPm = () => {
   ele.innerText = midDayMarker;
 }
 
+const createCalendar = () => {
+  const calendarContainer = document.querySelector(".calendar-container");
+  const now = new Date();
+  const month = now.toLocaleString('default', { month: 'long' });
+
+  const monthTitle = document.querySelector(".month-title");
+  monthTitle.innerText = month;
+
+  const firstDay = new Date(now.getFullYear(), now.getMonth(), 1);
+  const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+
+  console.log(firstDay.getDay());
+
+  let week = document.createElement("div");
+  week.className = "week";
+
+  for (let i = 0; i < firstDay.getDay(); i++) {
+    const day = document.createElement("div");
+    day.className = "day day--inactive";
+
+    week.append(day);
+  }
+
+  for (let i = 1; i < lastDay.getDate(); i++) {
+    const day = document.createElement("div");
+    day.className = "day";
+    day.innerText = i;
+
+    week.append(day);
+    if (week.children.length === 7) {
+      calendarContainer.append(week);
+      week = document.createElement("div");
+      week.className = "week";
+    }
+  }
+
+  for (i = lastDay.getDay(); i < 7; i++) {
+    const day = document.createElement("div");
+    day.className = "day day--inactive";
+
+    week.append(day);
+  }
+
+  calendarContainer.append(week);
+
+}
+
 setInterval(setClock, 1000);
 setAmPm();
 setClock();
+createCalendar();
